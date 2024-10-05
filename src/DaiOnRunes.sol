ab568b02cd51c953e05a21e1b868f5a3043e4edb
PirateCode <piratecodeweb3@gmail.com>
2024-10-05 20:08:56 +1000

    add reentrant protections

--- src/DaiOnRunes.sol
+++ src/DaiOnRunes.sol
@@ -5,13 +5,15 @@ import {IDaiOnRunes} from "./IDaiOnRunes.sol";
 import {Initializable} from "../lib/openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
 import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ERC165} from "../lib/openzeppelin-contracts/contracts/utils/introspection/ERC165.sol";
+import {ReentrancyGuard} from "../lib/openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
+
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {Dai} from "../lib/dss/src/dai.sol";
 
 /**
  * @title Bridge Ethereum Dai to Bitcoin Runes
  */
-contract DaiOnRunes is IDaiOnRunes, Ownable, ERC165, Initializable {
+contract DaiOnRunes is IDaiOnRunes, Ownable, ERC165, Initializable, ReentrancyGuard {
     uint256 constant MAX_FEE = 10 * 1e18;
     uint256 constant DEFAULT_FEE = 2 * 1e18;
 
@@ -33,21 +35,21 @@ contract DaiOnRunes is IDaiOnRunes, Ownable, ERC165, Initializable {
         redeemFee = DEFAULT_FEE;
     }
 
-    function mint(string calldata bitcoinAddress, uint256 amount) external {
+    function mint(string calldata bitcoinAddress, uint256 amount) external nonReentrant {
         require(amount > mintFee, "DaiOnRunes: mint amount less than mint fee");
         fee += mintFee;
         dai.transferFrom(msg.sender, address(this), amount);
         emit Minted(msg.sender, bitcoinAddress, amount, mintFee);
     }
 
-    function redeem(string calldata bitcoinTxId, address receiver, uint256 amount) external onlyOwner {
+    function redeem(string calldata bitcoinTxId, address receiver, uint256 amount) external onlyOwner nonReentrant {
         require(amount > redeemFee, "DaiOnRunes: redeem amount less than redeem fee");
         fee += redeemFee;
         dai.transferFrom(address(this), receiver, amount - redeemFee);
         emit Redeemed(bitcoinTxId, receiver, amount, redeemFee);
     }
 
-    function withdrawFee(uint256 amount) external onlyOwner {
+    function withdrawFee(uint256 amount) external onlyOwner nonReentrant {
         require(amount <= fee, "DaiOnRunes: withdraw amount more than fee");
         dai.transferFrom(address(this), owner(), amount);
         fee -= amount;
